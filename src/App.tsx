fcb7320724709797dcd1877b7547d268c1fb725e
America09 <americacarrasco208@gmail.com>
2024-06-12 12:50:45 -0500

    Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/Header';
-
+import Footer from './Components/Footer';
 const App: React.FC = () => {
   return (
     <div className="App">
@@ -10,6 +10,7 @@ const App: React.FC = () => {
         <h2>Bienvenido a mi aplicación</h2>
         <p>Esta es una aplicación de ejemplo creada con React y TypeScript.</p>
       </main>
+      <Footer />
     </div>
   );
 }
