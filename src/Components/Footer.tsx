97466b3b674ab2cbb09fb1ec37a006eb4b55de3e
America09 <americacarrasco208@gmail.com>
2024-06-12 18:15:55 -0500

    holi

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
 
 const Footer = () => {
-  return <footer>
-    <div >
+return (
+    <footer className="bg-gray-800 py-8 ">
+    <div className="container mx-auto text-center">
+        <div className="grid grid-cols-1 md:grid-cols-1 gap-4">
+        <div>
+            <h3 className="text-xl font-bold mb-2 text-white">NAVEGACIÓN</h3>
+            <ul className="text-sm text-white mb-4">
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Acerca de</a></li>
+                <li><a href="#">Servicios</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+            </div>
+        <div className="text-white mt-4">
+            <p className="mb-4">Footer prueba y error</p>
+            <p className="mb-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p className="mb-4">IDYGS91 2024 Ame</p>
+        </div>
+        <div>
+        </div>
+        </div>
     </div>
     </footer>
-};
+);
+}
 
 export default Footer;
+
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,15 +2,13 @@ import React from 'react';
 
 const Header: React.FC = () => {
   return (
-    <header className="header">
+    <header className="flex ">
       <h1>Mi Aplicación en React con TypeScript</h1>
-      <nav>
         <ul>
           <li><a href="/">Inicio</a></li>
           <li><a href="/about">Acerca de</a></li>
           <li><a href="/contact">Contacto</a></li>
         </ul>
-      </nav>
     </header>
   );
 };
